a31866be9c8a514c43b83d6ccb9069c089b25b7b
Tim Smith <tsmith@chef.io>
2018-04-30 14:06:36 -0700

    Add readme and metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'chocolatey_source'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache-2.0'
+description      'Provides a chocolatey_source resource for managing Chocolatey sources.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
+supports         'windows'
+source_url       'https://github.com/chef-cookbooks/chocolatey_source'
+issues_url       'https://github.com/chef-cookbooks/chocolatey_source/issues'
+chef_version     '>= 13.0'
