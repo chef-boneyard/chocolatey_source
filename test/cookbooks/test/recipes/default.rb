c27c6b59147b97519275c043470f28f6c56ee55e
Tim Smith <tsmith@chef.io>
2018-05-01 15:55:24 -0700

    Release 1.1.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,8 +1,5 @@
 include_recipe 'chocolatey::default'
 
-# this is really a bug in the chocolatey cookbook
-windows_path 'C:\ProgramData\chocolatey\bin'
-
 chocolatey_source 'MySource' do
   source 'http://example.com/something'
   action :add
